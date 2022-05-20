f53f4aa20f43c566621709afa0c34f85dc7a2644
artemiy.krot <artemiy.krot@gmail.com>
2022-05-20 22:05:19 +0300

    second attempt after craeating go.mod file

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,13 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	inputBytes := []byte(input)
+	length := len(inputBytes)
+	for n := 0; n < length/2; n++ {
+		//Reverser a string in bytes
+		inputBytes[n], inputBytes[length-1-n] = inputBytes[length-1-n], inputBytes[n]
+	}
+	output = string(inputBytes)
+
 	return output
 }
