d80695ca3dbfa6d88c6da74bcb87dc1efcf18caf
artemiy.krot <artemiy.krot@gmail.com>
2022-05-20 22:46:24 +0300

    bytes representation is changed to rune representation

--- reverse_string.go
+++ reverse_string.go
@@ -2,16 +2,12 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
-	inputBytes := []byte(input)
-	length := len(inputBytes)
+	inputRunes := []rune(input)
+	length := len(inputRunes)
 	for n := 0; n < length/2; n++ {
 		//Reverser a string in bytes
-		inputBytes[n], inputBytes[length-1-n] = inputBytes[length-1-n], inputBytes[n]
+		inputRunes[n], inputRunes[length-1-n] = inputRunes[length-1-n], inputRunes[n]
 	}
-	output = string(inputBytes)
-<<<<<<< HEAD
-
-=======
->>>>>>> de8fc06f9fd06c34cdb175421e589b42ef40eb6d
+	output = string(inputRunes)
 	return output
 }
